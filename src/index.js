48258dfac7c7bbdf9888c9eaaf9c632ddb4ab694
sahilvaghasiya2003 <sahilvaghasiyaaa@gmail.com>
2024-08-04 10:15:34 +0530

    Added JWT create and verify logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { PORT } = require("./config/serverConfig");
 const bodyParser = require("body-parser");
 const apiRoutes = require('./routes/index');
 // const UserRepository = require('./repository/user-repository')
+//  const UserService = require('./services/user-service')
 
 const app = express();
 
@@ -16,8 +17,14 @@ const prepareAndStartServer = () => {
   app.listen(PORT, async() => {
     console.log("server started");
     // const repo = new UserRepository();
+    // const service = new UserService();
     // const response = await repo.getById(1);
     // console.log(response);
+    // const newToken = service.createToken({email:'sahil@gmail.com',id:5});
+    // console.log(newToken)
+    // const verify = service.verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhaGlsQGdtYWlsLmNvbSIsImlkIjo1LCJpYXQiOjE3MjI3NDYyMTIsImV4cCI6MTcyMjkxOTAxMn0.1thBYqx2eGTlzbqK4D3GpBJWKVRYYltwJnjNKN1Vv9M');
+    // console.log(verify)
+  
   });
 };
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,21 +1,41 @@
 const UserRepository = require("../repository/user-repository");
+const jwt = require("jsonwebtoken");
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserService {
   constructor() {
     this.userRepository = new UserRepository();
   }
 
-  async create(data){
+  async create(data) {
     try {
-        const user = await this.userRepository.create(data);
+      const user = await this.userRepository.create(data);
       return user;
     } catch (error) {
       console.log("wrong at service layer");
-      throw {error}
-        
+      throw { error };
     }
   }
 
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "2d" });
+      return result;
+    } catch (error) {
+      console.log("wrong in create token service layer");
+      throw { error };
+    }
+  }
+
+  verifyToken(token){
+    try {
+      const response = jwt.verify(token,JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("wrong in  token validation service layer");
+      throw { error };
+    }
+  }
 
 }
 
