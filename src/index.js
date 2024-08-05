ccc6cff4e3d1dd73ab13f7e773fed47d6d6fa991
sahilvaghasiya2003 <sahilvaghasiyaaa@gmail.com>
2024-08-05 18:29:24 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
 const bodyParser = require("body-parser");
-const apiRoutes = require('./routes/index');
+const apiRoutes = require("./routes/index");
 // const UserRepository = require('./repository/user-repository')
 //  const UserService = require('./services/user-service')
+const db = require("./models/index");
 
 const app = express();
 
 const prepareAndStartServer = () => {
-
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  
+
   app.use("/api", apiRoutes);
 
-  app.listen(PORT, async() => {
+  app.listen(PORT, async () => {
     console.log("server started");
     // const repo = new UserRepository();
     // const service = new UserService();
@@ -24,7 +24,11 @@ const prepareAndStartServer = () => {
     // console.log(newToken)
     // const verify = service.verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhaGlsQGdtYWlsLmNvbSIsImlkIjo1LCJpYXQiOjE3MjI3NDYyMTIsImV4cCI6MTcyMjkxOTAxMn0.1thBYqx2eGTlzbqK4D3GpBJWKVRYYltwJnjNKN1Vv9M');
     // console.log(verify)
-  
+
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ after: true });
+    }
+
   });
 };
 
